623a8cf5cf831aa8e50a32eff930d8bafb5eb827
dkyle1mu <dkyle1@live.maryville.edu>
2025-10-19 07:41:29 -0500

    Delete app/api/firebaseSettings.tsx

--- app/api/firebaseSettings.tsx
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDyU_4qEGKLOME1lyUOIkFhRqASYF_BNAs",
-  authDomain: "garden-helper-67979.firebaseapp.com",
-  projectId: "garden-helper-67979",
-  storageBucket: "garden-helper-67979.firebasestorage.app",
-  messagingSenderId: "598957362353",
-  appId: "1:598957362353:web:05b44e3498f1885a542690"
-};
